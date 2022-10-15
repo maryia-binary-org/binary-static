58aeaa4e3506840d88e63a8feb6cf89e95af4910
maryia-binary <maryia@binary.com>
2022-10-15 13:39:13 +0300

    clean branch

--- src/templates/_common/components/title.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { getBrandName } from '../brand.config';
-
-const Title = () => (
-    <title>{`${it.title ? `${it.L(it.title)} | ` : ''}${it.L('Online trading platform')} | ${getBrandName()}`}</title>
-);
-
-export default Title;
